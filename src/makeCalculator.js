ba430ab03f82fa6ecccf84418227f3ac5c843eb1
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-06 19:40:34 +0300

    task done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,55 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  function operate(callback, value) {
+    result = callback(value);
+
+    return this;
+  }
+
+  function reset() {
+    result = 0;
+
+    return this;
+  }
+
+  function add(value) {
+    result += value;
+
+    return result;
+  }
+
+  function subtract(value) {
+    result -= value;
+
+    return result;
+  }
+
+  function multiply(value) {
+    result *= value;
+
+    return result;
+  }
+
+  function divide(value) {
+    result /= value;
+
+    return result;
+  }
+
+  return {
+    get result() {
+      return result;
+    },
+    add,
+    subtract,
+    multiply,
+    divide,
+    operate,
+    reset,
+  };
 }
 
 module.exports = makeCalculator;
